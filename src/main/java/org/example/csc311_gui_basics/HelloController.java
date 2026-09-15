3832de2a7cc6e440be44553bccbc19fe25e2a41c
Carlos Gonzalez <carlosgonzal100@gmail.com>
2026-09-15 00:08:55 -0400

    created a section in the update colors section in helloController.java class that changes the color scheme to a dark color scheme once the NO button has more clicks than the YES button

--- src/main/java/org/example/csc311_gui_basics/HelloController.java
+++ src/main/java/org/example/csc311_gui_basics/HelloController.java
@@ -64,10 +64,17 @@ public class HelloController {
      * and yesCounter label color to #046307. then make the background of the whole window to #B1f2FF
      */
 
+    /* if the no button has been clicked more than the yes button, change the color of the name
+     * label to #860111, the question label to #FA4113, the yes and no button to #B1f2FF, the text inside
+     * each button to gold, change the yes and no counter labels to white, then make the background of the
+     * whole window black
+     *
+     */
+
     private void updateColors() {
 
+        //if yes has more clicks changes to a heavenly color scheme
         if (count1 > count2) {
-            // YES has more votes
             nameText.setStyle("-fx-text-fill: #7E7CD9;");
             questionText.setStyle("-fx-text-fill: #FFFD01;");
 
@@ -85,6 +92,27 @@ public class HelloController {
             noCounter.setStyle("-fx-text-fill: #046307;");
 
             mainPane.setStyle("-fx-background-color: #B1F2FF;");
+        }
+        //if NO has more clicks, changes to a darker color scheme
+        else if (count2 > count1) {
+            nameText.setStyle("-fx-text-fill: #860111;");
+            questionText.setStyle("-fx-text-fill: #FA4113;");
+
+            yesButton.setStyle(
+                    "-fx-background-color: #B1F2FF;" +
+                            "-fx-text-fill: gold;"
+            );
+
+            noButton.setStyle(
+                    "-fx-background-color: #B1F2FF;" +
+                            "-fx-text-fill: gold;"
+            );
+
+            yesCounter.setStyle("-fx-text-fill: white;");
+            noCounter.setStyle("-fx-text-fill: white;");
+
+            mainPane.setStyle("-fx-background-color: black;");
+
         } else {
             // Counts are equal OR NO has more votes
             nameText.setStyle("-fx-text-fill: gold;");
